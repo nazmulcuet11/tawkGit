958516b554b92568b0a539ad15d6192a517db1d5
Nazmul Islam <nazmulcuet11@gmail.com>
2021-09-18 14:25:39 +0600

    add Custom navigation controller

--- tawkGit/App/AppDelegate.swift
+++ tawkGit/App/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         let window = UIWindow()
-        window.rootViewController = ViewController()
+        window.rootViewController = NavigationController(rootViewController: UserListVC())
         window.makeKeyAndVisible()
 
         self.window = window
---
+++ tawkGit/View/NavigationController.swift
@@ -0,0 +1,10 @@
+//
+//  NavigationController.swift
+//  tawkGit
+//
+//  Created by Nazmul Islam on 18/9/21.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController { }
